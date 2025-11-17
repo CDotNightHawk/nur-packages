31f8f30ed670ca0a45a47dcff603844c3dedf892
NightHawk <64332203+CDotNightHawk@users.noreply.github.com>
2025-11-17 16:38:26 -0500

    Implement mkSimpleLinks function in default.nix
    Added mkSimpleLinks function to create symbolic links.

--- lib/default.nix
+++ lib/default.nix
@@ -1,7 +1,25 @@
 { pkgs }:
 
-with pkgs.lib; {
-  # Add your library functions here
-  #
-  # hexint = x: hexvals.${toLower x};
+with pkgs.lib;
+{
+  mkSimpleLinks =
+    { name, links }:
+    pkgs.stdenvNoCC.mkDerivation {
+      inherit name;
+      preferLocalBuild = true;
+
+      dontUnpack = true;
+      dontPatch = true;
+      dontUpdateAutotoolsGnuConfigScripts = true;
+      dontConfigure = true;
+      dontBuild = true;
+      dontFixup = true;
+
+      installPhase = ''
+        mkdir $out
+      ''
+      + concatStringsSep "\n" (
+        mapAttrsToList (k: v: "ln -s ${info "evaluating ${k}" v} $out/${k}") links
+      );
+    };
 }
